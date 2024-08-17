f30973d3e5fd8f0f38db62ee47b557801afdf2c5
Dima Saveliev <dp236955@gmail.com>
2024-08-18 00:48:09 +0300

    Add and initialize redis storage for FSM context and caching

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,6 +4,8 @@ import os
 import sys
 
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+from redis.asyncio import Redis
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from bot.common.handlers import router
@@ -11,12 +13,12 @@ from bot.middlewares.db import DbSessionMiddleware
 
 load_dotenv()
 bot = Bot(token=os.getenv('TOKEN'))
-
+redis_fsm = Redis(host='localhost', port=6379)
 
 async def main():
     engine = create_async_engine(url=os.getenv('DB-URL'), echo=True)
     sessionmaker = async_sessionmaker(engine, expire_on_commit=True)
-    dp = Dispatcher()
+    dp = Dispatcher(storage=RedisStorage(redis=redis_fsm))
     dp.update.middleware(DbSessionMiddleware(session_pool=sessionmaker))
     dp.include_routers(router)
     await dp.start_polling(bot)
