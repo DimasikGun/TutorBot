5a795505da83fda4ff446b8871c10b6c3454dffb
Dima Saveliev <dp236955@gmail.com>
2024-09-04 01:16:22 +0300

    Add 'Notes' table to store tutors' notes about students
    Also migration created

--- db/users.py
+++ db/users.py
@@ -38,3 +38,10 @@ class Parents(BalanceMixin, Users):
 class Students(BalanceMixin, DiscordMixin, Users):
     parent = Column(BigInteger, ForeignKey('parents.user_id'), nullable=True)
     grade = Column(Integer, nullable=True)
+
+
+class Notes(Users):
+    id = Column(Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
+    tutor = Column(BigInteger, ForeignKey('tutors.user_id'), nullable=False)
+    student = Column(BigInteger, ForeignKey('students.user_id'), nullable=False)
+    text = Column(String(256), nullable=False)
